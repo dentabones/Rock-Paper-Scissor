4a387638acf46dc3243917c9d6b055451fb831d5
dentabones <sh183610@gmail.com>
2022-12-06 17:42:38 -0500

    Add function to randomly generate computer choice

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,22 @@
 "use strict";
 
+//function get prompts user to choose between rock paper or scissor
 function getUserChoice() {
   let userChoice = prompt("Choose rock, paper, or scissor");
   return userChoice.toLowerCase();
 }
 
-let userChoice = getUserChoice();
+//gets computers choice between rock paper and scissor
+function getComputerChoice() {
+  let indexNum = Math.floor(Math.random() * 3) + 1;
+
+  if (indexNum === 1) {
+    return "rock";
+  } else if (indexNum === 2) {
+    return "paper";
+  } else if (indexNum === 3) {
+    return "scissor";
+  } else return "something went wrong";
+}
+
+console.log(getComputerChoice());
