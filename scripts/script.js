72d694a221ca5204b9976dc199745f7f12332c17
dentabones <sh183610@gmail.com>
2022-12-09 14:30:48 -0500

    Refactor and Add reset after 5 rounds

--- scripts/script.js
+++ scripts/script.js
@@ -5,18 +5,41 @@ const playerScore = document.querySelector(".player-score");
 const computerScore = document.querySelector(".computer-score");
 const currentRound = document.querySelector(".round");
 
+let round = 1;
+let userScored = 0;
+let computerScored = 0;
+
 //returns value depending on which button is clicked
 function getUserChoice(e) {
-  if (this.classList.value === "rock") {
-    return "rock";
-  } else if (this.classList.value === "paper") {
-    return "paper";
-  } else if (this.classList.value === "scissor") {
-    return "scissor";
+  let userChoice;
+  let computerChoice = getComputerChoice();
+
+  if (this.className === "rock") {
+    userChoice = "rock";
+  } else if (this.className === "paper") {
+    userChoice = "paper";
+  } else if (this.className === "scissor") {
+    userChoice = "scissor";
+  }
+
+  let outcome = playRound(userChoice, computerChoice);
+
+  if (outcome === 0) {
+    computerScored++;
+    computerScore.textContent = computerScored;
+  } else if (outcome === 1) {
+    userScored++;
+    playerScore.textContent = userScored;
+  } else announcement.textContent = "Tie! No one wins this round";
+
+  round++;
+  currentRound.textContent = round;
+
+  if (userScored === 5 || computerScored === 5) {
+    announcement.textContent = `Final results: User - ${userScored} Computer - ${computerScored}`;
+    reset();
   }
 }
-
-butt.forEach((button) => button.addEventListener("click", getUserChoice));
 
 //gets computers choice between rock paper and scissor
 function getComputerChoice() {
@@ -28,54 +51,45 @@ function getComputerChoice() {
     return "paper";
   } else if (indexNum === 3) {
     return "scissor";
-  } else return "something went wrong";
+  }
 }
 
 //Compares user and computer choice, determines who wins
-function playRound() {
-  let computerChoice = getComputerChoice();
-  let userChoice = getUserChoice();
+function playRound(player, computer) {
+  let computerChoice = computer;
+  let userChoice = player;
   //return value for 0 and 1 used for tallying score in game function
   if (computerChoice === userChoice) {
-    return console.log("Tie! No one wins this round");
+    return;
   } else if (computerChoice === "rock" && userChoice === "scissor") {
-    console.log("You Lose! Rock beats scissor");
+    announcement.textContent = "You Lose! Rock beats scissor";
     return 0;
   } else if (computerChoice === "paper" && userChoice === "rock") {
-    console.log("You Lose! Paper beats rock");
+    announcement.textContent = "You Lose! Paper beats rock";
     return 0;
   } else if (computerChoice === "scissor" && userChoice === "paper") {
-    console.log("You Lose! Scissor beats paper");
+    announcement.textContent = "You Lose! Scissor beats paper";
     return 0;
   } else if (computerChoice === "rock" && userChoice === "paper") {
-    console.log("You win! Paper beats rock");
+    announcement.textContent = "You win! Paper beats rock";
     return 1;
   } else if (computerChoice === "paper" && userChoice === "scissor") {
-    console.log("You win! Scissor beats paper");
+    announcement.textContent = "You win! Scissor beats paper";
     return 1;
   } else if (computerChoice === "scissor" && userChoice === "rock") {
-    console.log("You win! Rock beats scissor");
+    announcement.textContent = "You win! Rock beats scissor";
     return 1;
   }
 }
 
-function game() {
-  let round = 1;
-  let userScored = 0;
-  let computerScored = 0;
+function reset() {
+  round = 1;
+  userScored = 0;
+  computerScored = 0;
 
-  for (let i = 0; i < 5; i++) {
-    let results = playRound();
-    if (results === 0) {
-      computerScored++;
-      computerScore.textContent = computerScored;
-    } else if (results === 1) {
-      userScored++;
-      playerScore.textContent = userScored;
-    }
-    round++;
-    currentRound.textContent = round;
-  }
-
-  console.log(`Final results: User - ${userScore} Computer - ${computerScore}`);
+  playerScore.textContent = userScored;
+  computerScore.textContent = computerScored;
+  currentRound.textContent = round;
 }
+
+butt.forEach((button) => button.addEventListener("click", getUserChoice));
